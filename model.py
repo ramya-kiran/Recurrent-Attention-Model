b841a93b9d12b93bdc117ba5c7160dce5d7f3b86
Ramya Rao <ramyarao@r-006.juliet.futuresystems.org>
2018-04-18 22:45:47 -0400

    first commit

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
-from config import *
+import tensorflow as tf
+# from config import *
+# tf.enable_eager_execution()
 
 '''
 This file contains the model used for the project
@@ -22,46 +28,46 @@ class Model:
     # the output of this function is the classifier output of the last LSTM cell 
     # class_outs is a 2D tensor of dimenstions [batch_size, number_of_claases]
     def __call__(self):
-        initial_means = tf.random_uniform([self.batch_size, LOC_DIM], minval=-1, maxval=1)
+        initial_locs = tf.random_uniform([self.batch_size, LOC_DIM], minval=-1, maxval=1)
         
         input_lstm = self.glimpse_network(self.inputs, initial_locs)
 
-        lstm_cell  = tf.nn.rnn_cell.LSTMCell(LSTM_HIDDEN, state_is_tuple=True)
-        init_state = lstm_cell.zero_state(self.batch_size, tf.float32)
-
-        extend_inputs = np.zeros((NUM_GLIMPSES, self.batch_size, GLIMPSE_FC2))
-        extend_inputs[0] = input_lstm
-
-        outputs, _ = tf.contrib.legacy_seq2seq.rnn_decoder(extend_inputs, init_state, 
-                                                               lstm_cell, loop_function=next_location)
+        collect_outputs= []
+        prev_output = tf.zeros([self.batch_size, LSTM_HIDDEN])
+        prev_state = tf.zeros([self.batch_size, LSTM_HIDDEN])
         
-        class_outs = self.fc_layer(outputs[-1], GLIMPSE_FC2, NUM_CLASSES, 'softmax', tf.nn.relu)
+        for i in range(NUM_GLIMPSES):
+            curr_out, next_state = self.lstm_layer(prev_output, prev_state, input_lstm)
+            collect_outputs.append(curr_out)
+            prev_output = self.next_location(curr_out, i)
+            prev_state = next_state
+            
+        class_outs = self.fc_layer(collect_outputs[-1], GLIMPSE_FC2, NUM_CLASSES, 'softmax', tf.nn.relu)
 
-        return outputs, tf.nn.softmax(class_outs), self.collect_means, self.collect_locs
+        return collect_outputs, tf.nn.softmax(class_outs), self.collect_means, self.collect_locs
     
 
     # This function is called by the current LSTM cell to get inputs to the next cell
     # next_inputs are of dimension [batch_size, 256]
     def next_location(self, prev_inputs, i):
-        
-        with tf.variable_scope('next_loc'):
+        with tf.variable_scope('next_loc', reuse=tf.AUTO_REUSE):
             weights = tf.get_variable("weights", [GLIMPSE_FC2, LOC_DIM], initializer=tf.contrib.layers.xavier_initializer())
             biases = tf.get_variable("biases", [LOC_DIM], initializer=tf.contrib.layers.xavier_initializer())
-            y = tf.add(tf.matmul(image, weights), biases)
+            y = tf.add(tf.matmul(prev_inputs, weights), biases)
             y = tf.stop_gradient(y)
             
-        mean = tf.clip_by_value(y, -1., 1.)
+        means = tf.clip_by_value(y, -1., 1.)
         means = tf.stop_gradient(means)
 
         self.collect_means.append(means)
 
-        dist = tf.contrib.distributions.Normal(mu=locs, sigma=np.ones((self.batch_size, 2)))
-        locs = tf.clip_by_value(dist.sample([1]), -1.0, 1.0)
+        dist = tf.contrib.distributions.Normal(means, np.ones((self.batch_size, 2), dtype=np.float32))
+        locs = tf.squeeze(tf.clip_by_value(dist.sample([1]), -1.0, 1.0))
 
-        self.collect_locs(locs)
+        self.collect_locs.append(locs)
 
         next_inputs = self.glimpse_network(self.inputs, locs)
-
+        
         return next_inputs
 
 
@@ -80,13 +86,14 @@ class Model:
         g_out1 = self.fc_layer(glimpses, G_DIM, GLIMPSE_FC1, 'g1', tf.nn.relu)
         g_out2 = self.fc_layer(g_out1, GLIMPSE_FC1, GLIMPSE_FC2, 'g2', tf.nn.relu)
 
+
         return tf.nn.relu(loc_out2 + g_out2)
 
 
     # general template for a fully connected layer used by the model
     # output dimensions are [batch_size, out_size]
     def fc_layer(self, image, in_size, out_size, name, activation):
-        with tf.variable_scope(name):
+        with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
             weights = tf.get_variable("weights", [in_size, out_size], initializer=tf.contrib.layers.xavier_initializer())
             biases = tf.get_variable("biases", [out_size], initializer=tf.contrib.layers.xavier_initializer())
             y = tf.add(tf.matmul(image, weights), biases)
@@ -98,4 +105,40 @@ class Model:
 
             return y
     
+    
+    def lstm_layer(self, last_state, last_outout, curr_input):
+        
+        with tf.variable_scope('lstm_cell', reuse=tf.AUTO_REUSE):
+            whprev = tf.get_variable("whprev", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            wx = tf.get_variable("wxcurr", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            wf = tf.get_variable("wf", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            bf = tf.get_variable("bf", [GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            
+            wi = tf.get_variable("wi", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            bi = tf.get_variable("bi", [GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            
+            wc = tf.get_variable("wc", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            bc = tf.get_variable("bc", [GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            
+            wo = tf.get_variable("wo", [GLIMPSE_FC2, GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            bo = tf.get_variable("bo", [GLIMPSE_FC2], initializer=tf.contrib.layers.xavier_initializer())
+            
+            main_mix = tf.matmul(curr_input, whprev)+ tf.matmul(curr_input, wx)
+            
+            ft = tf.nn.sigmoid(tf.add(tf.matmul(main_mix, wf), bf))
+            
+            it = tf.nn.sigmoid(tf.add(tf.matmul(main_mix, wi), bi))
+            
+            cbart = tf.nn.tanh(tf.add(tf.matmul(main_mix, wc), bc))
+             
+            ct = tf.multiply(ft, last_state) + tf.multiply(it, cbart)
+            
+            ot = tf.nn.sigmoid(tf.add(tf.matmul(main_mix, wo), bo))
+            
+            ht = tf.multiply(ot, tf.nn.tanh(ct))
+            
+            return ht, ct
+            
+            
+        
         
--- train.py
+++ train.py
@@ -8,27 +8,34 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import os 
-from config import *
-from model import *
+# from config import *
+# from model import *
 from tensorflow.python.framework import ops
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-def train(train_tfrecords, batch_size, epochs, log, output):
+def train(batch_size, epochs, log, output):
     # Read arguments
-    # ops.reset_default_graph()
-    filename_queue = tf.train.string_input_producer([train_tfrecords])
-    image, label = read_and_decode(filename_queue)
-    batch = tf.train.shuffle_batch([image, label], batch_size=args.batch_size, capacity=100, num_threads=2, min_after_dequeue=40)
+    ops.reset_default_graph()
+    # filename_queue = tf.train.string_input_producer([train_tfrecords])
+    # image, label = read_and_decode(filename_queue)
+    # batch = tf.train.shuffle_batch([image, label], batch_size=args.batch_size, capacity=100, num_threads=2, min_after_dequeue=40)
+    
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
     # placeholders for the input and labels
-    X = tf.placeholder(tf.float32, [None, INPUT_SIZE], name='X')
+    X = tf.placeholder(tf.float32, [None, IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH], name='X')
     y = tf.placeholder(tf.float32, [None, 10], name='labels')
 
     # Model instantiated and called for processing the inputs
     model = Model(X, batch_size)
     lstm_output, y_hat, collect_means, collect_locs = model()
+
+    
+    outputs = tf.transpose(lstm_output, [0,1,2])
+    means= tf.transpose(collect_means, [0,1,2])
+    locs= tf.transpose(collect_locs, [0,1,2])
 
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
         labels=y,
@@ -37,7 +44,8 @@ def train(train_tfrecords, batch_size, epochs, log, output):
 
     correct_prediction = tf.equal(tf.argmax(y_hat, 1), tf.argmax(y, 1))
 
-    rewards = tf.cast(correct_prediction, tf.float32)
+    rewards = tf.expand_dims(tf.cast(correct_prediction, tf.float32), 1)
+#     print(rewards.shape)
 
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -45,14 +53,19 @@ def train(train_tfrecords, batch_size, epochs, log, output):
     Calculate loss and then define the Optimizer and related hyper-parameters
     '''
 
-    b_t = tf.reduce_mean(rewards) # 1 x T tensor
+    b_t = tf.reduce_mean(rewards, 0) # 1 x T tensor
     
-    gradient = tf.reduce_mean(tf.reduce_mean(tf.tensordot(tf.multiply(tf.transpose(lstm_output, perm=[2, 0, 1]),
+    gradient = tf.reduce_mean(tf.reduce_mean(tf.tensordot(tf.multiply(tf.transpose(outputs, perm=[2, 0, 1]),
                                                                       tf.subtract(rewards, b_t)), 
-                                                          tf.subtract(collect_locs, collect_means),
+                                                          tf.subtract(locs, mean),
                                                           axes=[[2], [1]]),
                                              axis=1), 
-                              axis=1) # a [K x 2] tensor
+                              axis=1)
+    
+    print(gradient.shape)
+    vars_location = tf.get_collection(tf.GraphKeys.VARIABLES, scope= "next_loc")
+
+    train = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
     saver = tf.train.Saver()
 
@@ -69,17 +82,19 @@ def train(train_tfrecords, batch_size, epochs, log, output):
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         # training the model for a predetermined number of epochs
         for i in range(epochs):
-            img, lbl = sess.run(batch)
-            acc = sess.run(y_hat, feed_dict={X: img, y: lbl})
-            s = sess.run(merged_summary, feed_dict={X: img, y: lbl})
+            # img, lbl = sess.run(batch)
+            batch_x, batch_lbl = mnist.train.next_batch(batch_size)
+            batch_x = batch_x.reshape((batch_size, 28, 28, 1))
+            acc = sess.run(accuracy, feed_dict={X: batch_x, y: batch_lbl})
+            s = sess.run(merged_summary, feed_dict={X: batch_x, y: batch_lbl})
             writer.add_summary(s, i)
 
             if (i+1) % 275 == 0:
-                print('Step {}: {:.2f}'.format(i+1, acc))
+                print('Step {}: {}'.format(i+1, acc))
 
             if (((i+1) % 275 == 0) and (acc > 0.99)):
                 # Please change the directory here to save the model in a different location
-                params = saver.save(sess, '/N/u/ramyarao/dl_hw2/exp_model/{}_{}.ckpt'.format(output, i+1))
+                params = saver.save(sess, '/N/u/ramyarao/project/model/{}_{}.ckpt'.format(output, i+1))
                 print('Model saved: {}'.format(params))
 
         coord.request_stop()
@@ -88,7 +103,8 @@ def train(train_tfrecords, batch_size, epochs, log, output):
         
 
 if __name__ == '__main__':
-    train('', 200, 275*10000, 'logs', 'model')
+    train(200, 275*10000, 'logs', 'model')
+
 
 
 
